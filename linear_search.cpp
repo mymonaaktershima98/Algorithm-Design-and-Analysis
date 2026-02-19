37f6eb72911b3a40a66a134c16ea069984eed05e
mymonaaktershima98 <mymonaaktershima98@gmail.com>
2026-02-19 20:18:06 +0600

    Create linear_search.cpp

---
+++ linear_search.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int n, int key)
+{
+    for(int i = 0; i < n; i++)
+    {
+        if(arr[i] == key)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int n;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int key;
+    cout << "Enter element to search: ";
+    cin >> key;
+
+    int result = linearSearch(arr, n, key);
+
+    if(result != -1)
+        cout << "Element found at index: " << result;
+    else
+        cout << "Element not found";
+
+    return 0;
+}
